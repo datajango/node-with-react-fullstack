3d9bff12c8efd3431c5f3d3c57bca565bfbf96db
Anthony L. Leotta <tony@leotta.com>
2018-08-27 05:14:05 -0400

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node With React Full Stack Udemy Course
+
+Started 8/23/2018
+
+## Prettier
+
+The purpose of prettier is to format and stylistically standardize code that you write.
+
+## This project uses Heroku
+
---
+++ server/index.js
@@ -0,0 +1,16 @@
+// index.js
+// Create by Anthony Leotta
+// 8/25/2018
+// -- using Common JS Modules
+
+const express = require('express'); // this is not a ES2015 import statement
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);

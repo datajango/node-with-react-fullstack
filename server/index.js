91dc8b056f2e32e995039da22b9cd6093ec223bc
Anthony L. Leotta <tony@leotta.com>
2018-08-27 05:42:00 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const express = require('express'); // this is not a ES2015 import statement
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'hello world'});
 })
 
 const PORT = process.env.PORT || 5000;

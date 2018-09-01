2722bad832d6350bc4a844f1a52359e4a336eeb8
Anthony L. Leotta <tony@leotta.com>
2018-09-01 08:38:10 -0400

    test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(
     cookieSession({
         // maxAge is how long the cookie can exist before it is expired
-        maxAge: 30 * 24 * 60 * 60 * 1000,  // 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         keys: [keys.cookieKey]
     })
 );
@@ -35,14 +35,14 @@ if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
-  
+
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+}
 
 
 const PORT = process.env.PORT || 5000;

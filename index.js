e24514c46bc53265de8840fd2d4f0d53a1360fff
Anthony L. Leotta <tony@leotta.com>
2018-09-01 20:54:57 -0400

    debugging

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+console.log('------------------------------------------------');
+
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
